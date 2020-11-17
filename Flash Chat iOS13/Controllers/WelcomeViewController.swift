d107fbeb335b9581a34f3f0ea1e6575eed948ca9
kanta-ishii <kn.is.100000000@gmail.com>
2020-11-18 02:48:50 +0900

    No.1 ・チャットアプリを作る

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Angela Yu on 21/10/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+import CLTypingLabel
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+//        titleLabel.text = ""
+//        let charIndex = 0.0
+//        let titleText = "⚡️FlashChat"
+//        for letter in titleText{
+//            print(charIndex)
+//            titleLabel.text?.append(letter)
+//        }
+        
+    }
+    
+
+}
