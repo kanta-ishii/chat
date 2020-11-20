8b5e79ce3224893c32c773c2edbd1467398e11c7
kanta-ishii <kn.is.100000000@gmail.com>
2020-11-21 06:51:31 +0900

    No.2 ・firebaseと繋げてログインする ・制作したアカウントを使いログインする

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,22 +7,23 @@
 //
 
 import UIKit
-import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: CLTypingLabel!
+    @IBOutlet weak var titleLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        titleLabel.text = ""
-//        let charIndex = 0.0
-//        let titleText = "⚡️FlashChat"
-//        for letter in titleText{
-//            print(charIndex)
-//            titleLabel.text?.append(letter)
-//        }
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false){(timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
         
     }
     
